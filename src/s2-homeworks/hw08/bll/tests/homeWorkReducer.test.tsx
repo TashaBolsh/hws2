6afd4c54e0e0be735975cd07db37ecbe3b93435b
TashaBolsh <SerdcevaTanyaN@gmail.ru>
2023-11-08 16:32:03 +0300

    Add new Array

--- src/s2-homeworks/hw08/bll/tests/homeWorkReducer.test.tsx
+++ src/s2-homeworks/hw08/bll/tests/homeWorkReducer.test.tsx
@@ -6,12 +6,16 @@ let initialState: UserType[]
 
 beforeEach(() => {
     initialState = [
-        { _id: 0, name: 'Кот', age: 3 },
+    /*    { _id: 0, name: 'Кот', age: 3 },
         { _id: 1, name: 'Александр', age: 66 },
         { _id: 2, name: 'Коля', age: 16 },
         { _id: 3, name: 'Виктор', age: 44 },
         { _id: 4, name: 'Дмитрий', age: 40 },
-        { _id: 5, name: 'Ирина', age: 55 },
+        { _id: 5, name: 'Ирина', age: 55 },*/
+        {_id: 0, name: 'Алданов Антон', age: 16},
+        {_id: 1, name: 'Бориславов Макс', age: 23},
+        {_id: 2, name: 'Тимошин Роман', age: 19},
+        {_id: 3, name: 'Макаров Михаил', age: 54},
     ]
 })
 
