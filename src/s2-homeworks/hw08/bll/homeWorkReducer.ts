92ff08a3c64cc3c55b92b63f44eed84d466aa1e2
TashaBolsh <SerdcevaTanyaN@gmail.ru>
2023-11-08 09:17:32 +0300

    Add hws2 3 jun

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,14 +4,21 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            state.sort((a, b) => {
+                if (a.name > b.name) {return 1}
+                if (a.name < b.name) {return -1}
+                return 0;
+            })
+            if(action.payload === 'down'){
+                state.reverse()
+            }
+            return state
         }
         case 'check': {
-
+            state = state.filter( (stud) => stud.age >= 18 )
             return state // need to fix
         }
         default:
