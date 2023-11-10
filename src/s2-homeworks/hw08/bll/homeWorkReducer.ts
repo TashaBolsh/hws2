73929030adf7437a3bb2578217802f1fe61c1a0e
TashaBolsh <SerdcevaTanyaN@gmail.ru>
2023-11-10 16:03:34 +0300

    Add new Reducer2

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,33 +4,29 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-let stateCopy
-
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-            stateCopy = [...state]
-            if(action.payload === 'up')
-            {
+            if(action.payload === 'down'){
+                state.reverse()
+            }
+            if(action.payload === 'up'){
             state.sort((a, b) => {
                 if (a.name > b.name) {return 1}
                 if (a.name < b.name) {return -1}
                 return 0;
             })}
-            else if(action.payload === 'down')
-            {
-                stateCopy.reverse()
-            }
-            return stateCopy
+            return state
         }
         case 'check': {
-            stateCopy = state.filter( (stud) => stud.age >= 18 )
-            stateCopy.sort((a, b) => {
+            state = state.filter( (stud) => stud.age >= 18 )
+        //////////////////////////////////////////////////////////////////////////
+           state.sort((a, b) => {
                 if (a.age > b.age) {return 1}
                 if (a.age < b.age) {return -1}
-                return 0;
-            })
-            return stateCopy // need to fix
+                return 0;})
+         ///////////////////////////////////////////////////////////////////////
+            return state // need to fix
         }
         default:
             return state
