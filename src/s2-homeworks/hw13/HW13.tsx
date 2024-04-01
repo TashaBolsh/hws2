75c3d565859dd3acbf0dccad0b0cf7b116c27f79
TashaBolsh <SerdcevaTanyaN@gmail.ru>
2024-04-01 16:18:01 +0300

    Add hw13

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -7,6 +7,8 @@ import success200 from './images/200.svg'
 import error400 from './images/400.svg'
 import error500 from './images/500.svg'
 import errorUnknown from './images/error.svg'
+import {AppStoreType} from "../hw10/bll/store";
+import {useSelector} from 'react-redux'
 
 /*
 * 1 - дописать функцию send
@@ -19,13 +21,16 @@ const HW13 = () => {
     const [text, setText] = useState('')
     const [info, setInfo] = useState('')
     const [image, setImage] = useState('')
+    let state = useSelector<AppStoreType,{isLoading: boolean}>(state => state.loading)
 
     const send = (x?: boolean | null) => () => {
+
         const url =
             x === null
                 ? 'https://xxxxxx.ccc' // имитация запроса на не корректный адрес
                 : 'https://samurai.it-incubator.io/api/3.0/homework/test'
 
+        state.isLoading = true
         setCode('')
         setImage('')
         setText('')
@@ -34,14 +39,43 @@ const HW13 = () => {
         axios
             .post(url, {success: x})
             .then((res) => {
-                setCode('Код 200!')
-                setImage(success200)
-                // дописать
+                let isLoading = true
+                if(res.status === 200)
+                {
+                    setCode('Код 200!')
+                    setImage(success200)
+                    setText('...все ок)')
+                    setInfo('код 200 - обычно означает что скорее всего все ок)')
+                    state.isLoading=false
+                    return true
+                }
+
+                else if(send(false)) {
+                    setCode('Код 500!')
+                    setImage(error500)
+                    setText('эмитация ошибки на сервере)')
+                    setInfo('ошибка 500 - обычно означает что что-то сломалось не сервере, например база данных)')
+                    state.isLoading=false
+                    return false
+                }
+
+                else if(x === undefined)
+                {
+                    setCode('Код 400!')
+                    setImage(error400)
+                    setText('Ты не отправил success в body вообще!')
+                    setInfo('Ошибка 400 обычно означает что скорее всего форнт отправил что-то не то на бэк!')
+                    state.isLoading=false
+                    return undefined
+                }
 
             })
             .catch((e) => {
-                // дописать
-
+                setCode('Error!')
+                setImage(errorUnknown)
+                setText('Network')
+                setInfo('Error AxiosError')
+                state.isLoading=false
             })
     }
 
@@ -55,36 +89,28 @@ const HW13 = () => {
                         id={'hw13-send-true'}
                         onClick={send(true)}
                         xType={'secondary'}
-                        // дописать
-
-                    >
+                        disabled={state.isLoading}>
                         Send true
                     </SuperButton>
                     <SuperButton
                         id={'hw13-send-false'}
                         onClick={send(false)}
                         xType={'secondary'}
-                        // дописать
-
-                    >
+                        disabled={state.isLoading}>
                         Send false
                     </SuperButton>
                     <SuperButton
                         id={'hw13-send-undefined'}
                         onClick={send(undefined)}
                         xType={'secondary'}
-                        // дописать
-
-                    >
+                        disabled={state.isLoading}>
                         Send undefined
                     </SuperButton>
                     <SuperButton
                         id={'hw13-send-null'}
-                        onClick={send(null)} // имитация запроса на не корректный адрес
+                        onClick={send(null)}
                         xType={'secondary'}
-                        // дописать
-
-                    >
+                        disabled={state.isLoading}>
                         Send null
                     </SuperButton>
                 </div>
